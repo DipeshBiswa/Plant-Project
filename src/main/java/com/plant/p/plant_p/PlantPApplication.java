031a7aff6e38832b3cbd3a1fe66939c53100edba
Dipesh Biswa <db4048@g.rit.edu>
2026-08-25 13:39:38 -0400

    Initial commit from Spring Initializr

---
+++ src/main/java/com/plant/p/plant_p/PlantPApplication.java
@@ -0,0 +1,13 @@
+package com.plant.p.plant_p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantPApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plant/p/plant_p/PlantPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plant.p.plant_p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
